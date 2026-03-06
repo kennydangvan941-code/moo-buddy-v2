585825672541230fae4e12acbbfd6e3a10f5945e
DANG VAN KIEN <kennydangvan941@gmail.com>
2026-03-06 10:59:35 +0700

    Update IUserRepository interface to include methods for user management

--- MooBuddy/MooBuddy.Application/Common/Interfaces/IUserRepository.cs
+++ MooBuddy/MooBuddy.Application/Common/Interfaces/IUserRepository.cs
@@ -1,11 +1,14 @@
 using MooBuddy.Domain.Entities;
-using System.Linq.Expressions;
 
 namespace MooBuddy.Application.Common.Interfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository : IBaseRepository<User>
     {
-        Task<bool> AnyAsync(Expression<Func<User, bool>> predicate);
-        void Add(User user);
+        Task<User?> FindByEmailAsync(string email);
+        Task<bool> EmailExistsAsync(string email);
+        Task<IEnumerable<User>> GetActiveUsersAsync();
+        Task<IEnumerable<User>> GetByStatusAsync(MooBuddy.Domain.Enums.UserStatus status);
+        Task<User?> FindByIdWithRelationsAsync(Guid id);
+        Task<IEnumerable<User>> GetAllWithRelationsAsync();
     }
 }
